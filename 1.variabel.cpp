fa01bcd773e6db42c38fab604fb9f1157fbbeffc
Theikol <ikoladrian@gmail.com>
2026-06-11 06:45:08 +0700

    firstVariabel

---
+++ 1.variabel.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+	string yapping="My Favorit Car is ";
+	string favCar="bmwE28";
+	cout << yapping + favCar << endl;
+	return 0;
+}
