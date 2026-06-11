7f47b2924cdb8b4b321035e853117880e6891b18
Theikol <ikoladrian@gmail.com>
2026-06-11 07:02:37 +0700

    lastets variable

--- 1.variabel.cpp
+++ 1.variabel.cpp
@@ -3,8 +3,17 @@
 using namespace std;
 
 int main(){
+	int i = 15;
+	int j = 11;
+	int k = i+j;
 	string yapping="My Favorit Car is ";
 	string favCar="bmwE28";
 	cout << yapping + favCar << endl;
+	
+	cout << "Jumlah dari i dan j adalah " << i+j << endl;
+	cout << "Pengurangan dari " << i << " dan " << j << " adalah " << i-j << endl;
+	cout << "Perkalian dari " << i << " dan " << j << " adalah " << i*j << endl;
+	cout << "Pembagian dari " << i << " dan " << j << " adalah " << i/j << endl;
+	cout << "Sisa bagi dari " << i << " dan " << j << " adalah " << i%j << endl;
 	return 0;
 }
